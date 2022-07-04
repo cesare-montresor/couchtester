6dc695757e86d8342a45ce1daa96f7379e408167
Cesare Montresor <cesare.montresor@gmail.com>
2022-07-04 18:35:58 +0200

    almost there...

--- import.py
+++ import.py
@@ -45,7 +45,6 @@ class CouchVeronaCard():
 
         self.coll_import_name = "import"
         self.coll_poi_name = "vc_poi"
-        self.coll_time_name = "vc_time"
         self.coll_card_name = "vc_card"
         self.coll_log_name = "vc_log"
 
@@ -55,7 +54,6 @@ class CouchVeronaCard():
 
         self.coll_import = None
         self.coll_poi = None
-        self.coll_time = None
         self.coll_card = None
         self.coll_log = None
 
@@ -81,7 +79,6 @@ class CouchVeronaCard():
 
         self.coll_import = self.scope.collection(self.coll_import_name)
         self.coll_poi = self.scope.collection(self.coll_poi_name)
-        self.coll_time = self.scope.collection(self.coll_time_name)
         self.coll_card = self.scope.collection(self.coll_card_name)
         self.coll_log = self.scope.collection(self.coll_log_name)
 
@@ -95,6 +92,10 @@ class CouchVeronaCard():
                     self.coll_import.upsert(key, doc.toDictionary())
                 except Exception as e:
                     print(e)
+
+    def LogDataImport(self):
+
+
 
     def AggregateByVenueDate(self):
         """
@@ -155,22 +156,91 @@ class CouchVeronaCard():
             print(e)
         print(cntr)
 
-    def searchByCard(self):
+    def searchVenueLestVisitDay(self, day):
         """
         1- POI che ha avuto il minimo numero di accessi in un giorno specifico.
+        """
+        qry = """
+            SELECT p.*
+            FROM verona_card.verona_card_0.vc_poi p
+            WHERE 
+            date = $1 AND cnt WITHIN (
+                SELECT cnt
+                FROM verona_card.verona_card_0.vc_poi p2
+                WHERE 
+                    date = $1
+                ORDER BY cnt
+                LIMIT 1
+            )
+        """
+        try:
+            params = QueryOptions(positional_parameters=[day])
+            row_iter = self.cluster.query(qry, params)
+            for row in row_iter:
+                print(row)
+        except Exception as e:
+            print(e)
+
+    def searchVenueMostMonthVisitYear(self, year):
+        """
         2- dato un anno, trovare per ogni mese il POI con il numero massimo di accessi.
+        """
+        qry = """
+            SELECT p.*
+            FROM verona_card.verona_card_0.vc_poi p
+            WHERE 
+            year = $1 AND (
+                ( month = '01' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '01' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '02' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '02' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '03' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '03' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '04' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '04' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '05' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '05' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '06' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '06' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '07' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '07' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '08' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '08' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '09' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '09' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '10' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '10' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '11' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '11' ORDER BY cnt DESC LIMIT 1 ) ) OR 
+                ( month = '12' AND cnt WITHIN ( SELECT cnt FROM verona_card.verona_card_0.vc_poi p1 WHERE year = $1 AND month = '12' ORDER BY cnt DESC LIMIT 1 ) ) 
+            )
+            ORDER BY month
+        """
+        try:
+            params = QueryOptions(positional_parameters=[year])
+            row_iter = self.cluster.query(qry, params)
+            for row in row_iter:
+                print(row)
+        except Exception as e:
+            print(e)
+
+    def searchCardsSwipeMultipleDays(self):
+        """
         3- le verona card che hanno fatto strisciate in più di 1 giorno, riportando tutte le strisciate che la carta ha eseguito.
         """
+        qry = """
+            SELECT c.*
+            FROM verona_card.verona_card_0.vc_card c
+            WHERE num_days > 1
+            ORDER BY cnt DESC
+        """
+        try:
+            row_iter = self.cluster.query(qry)
+            for row in row_iter:
+                print(row)
+        except Exception as e:
+            print(e)
 
 def main():
     cvc = CouchVeronaCard("Administrator", "123456", "verona_card", 'verona_card_0')
     cvc.connect()
+
     csvpath = "./dataset_veronacard_2014_2020/dati_2014.csv"
     # cvc.importCSV(csvpath)
     #cvc.AggregateByVenueDate()
-    cvc.AggregateByCard()
-
-
+    #cvc.AggregateByCard()
+    #cvc.searchVenueLestVisitDay('17-08-14')
+    #cvc.searchVenueMostMonthVisitYear('14')
+    cvc.searchCardsSwipeMultipleDays()
 
 
 
